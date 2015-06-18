b56cf6975f8f0164a637042dd91ca13f07b60c92
yurykorzun <paranoekk@gmail.com>
2015-06-18 16:56:59 -0400

    some minor cleanups

--- app/mailers/thing_mailer.rb
+++ app/mailers/thing_mailer.rb
@@ -1,5 +1,5 @@
 class ThingMailer < ActionMailer::Base
-  default from: 'adoptahydrant@cityofboston.gov'
+  default from: 'adoptahydrant@cityofphiladelphia.gov'
 
   def reminder(thing)
     @thing = thing
